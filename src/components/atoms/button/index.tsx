9b4bf602cc45c99472c0cd657c7041cb23adecfd
kameda <y-kameda@aeonlink.co.jp>
2020-06-09 16:14:59 +0900

    feat: add button component

---
+++ src/components/atoms/button/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+import styles from "./style.module.scss"; // Import css modules stylesheet as styles
+
+const Button: FC = () => <button className={styles.button}>Button</button>;
+
+export default Button;
